c05b18db21c0a78727063e35891a441e5daee336
WalrusCow <willgmcd@gmail.com>
2015-06-19 11:47:25 -0400

    Bind socket

--- Makefile
+++ Makefile
@@ -4,7 +4,7 @@ DEBUG=-g
 CPPFLAGS=-std=c++1y -pedantic -Wall -Wextra -Wcast-align -Wcast-qual \
 -Wctor-dtor-privacy -Wdisabled-optimization -Wformat=2 -Winit-self \
 -Wlogical-op -Wmissing-declarations -Wmissing-include-dirs -Wnoexcept \
--Wold-style-cast -Woverloaded-virtual -Wredundant-decls -Wshadow \
+-Woverloaded-virtual -Wredundant-decls -Wshadow \
 -Wsign-conversion -Wsign-promo -Wstrict-null-sentinel -Wstrict-overflow=5 \
 -Wswitch-default -Wundef -Werror -Wno-unused
 
--- server.cpp
+++ server.cpp
@@ -2,7 +2,22 @@
 
 #include <cctype>
 #include <iostream>
+#include <cstdlib>
 #include <sstream>
+
+#include <netinet/in.h>
+#include <sys/select.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+
+namespace {
+
+void fatalError(const std::string& error, int exitCode) {
+  std::cerr << error << std::endl;
+  std::exit(exitCode);
+}
+
+} // Anonymous
 
 std::string titleCase(const std::string& inString) {
   bool toUpper = true;
@@ -17,6 +32,21 @@ std::string titleCase(const std::string& inString) {
 }
 
 int main(void) {
-  std::string s = titleCase("");
-  std::cout << s << std::endl;
+  int sfd = socket(AF_INET, SOCK_STREAM, 0);
+  struct sockaddr_in serveAddr;
+  if (sfd == -1) {
+    fatalError("Failed to create socket", -1);
+  }
+
+  serveAddr.sin_family = AF_INET;
+  serveAddr.sin_addr.s_addr = INADDR_ANY;
+  serveAddr.sin_port = 0; // Next available
+
+  if (bind(sfd, (struct sockaddr*) (&serveAddr), sizeof(serveAddr)) == -1) {
+    fatalError("Failed to bind socket", -2);
+  }
+
+  while(1) {
+    
+  }
 }
